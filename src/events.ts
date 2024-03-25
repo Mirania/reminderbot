38bba7c0a03b897812d145f576c58c253fa2f839
Mirania <blakbgtr@gmail.com>
2024-03-25 17:13:42 +0000

    fix renew bug

--- src/events.ts
+++ src/events.ts
@@ -39,6 +39,7 @@ function renewReminder(reminder: data.Reminder): void {
         switch (unit) {
             case "year": case "y": date.add(value, "year"); break;
             case "month": case "mo": date.add(value, "month"); break;
+            case "week": case "w": date.add(value, "week"); break;
             case "day": case "d": date.add(value, "day"); break;
             case "hour": case "h": date.add(value, "hour"); break;
             case "minute": case "m": date.add(value, "minute"); break;
