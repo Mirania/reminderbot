2a268c0703dab0c6131e425d0eca08e694930a46
Mirania <blakbgtr@gmail.com>
2025-08-19 19:52:20 +0100

    log battery status

--- src/battery.ts
+++ src/battery.ts
@@ -1,8 +1,12 @@
 import * as cp from "child_process";
+import * as utils from './utils';
 
 export function getBatteryStatus(): Promise<{ percentage: number, isCharging: boolean }> {
     return new Promise((resolve, reject) => {
+        utils.log("Querying battery statys...");
+
         cp.exec("termux-battery-status", (error, stdout, stderr) => {
+            utils.log(`Got raw output:\nerror -> ${error}\nstderr -> ${stderr}\nstdout -> ${stdout}`);
             if (error) { reject("Got an exec exception:\n" + error); return; }
             if (stderr) { reject("Got stderr:\n" + stderr); return; }
 
